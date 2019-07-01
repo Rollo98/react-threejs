3e43950df781b8d88f60b501b81c0927ccbff3bb
Rolleh <32469870+Rollo98@users.noreply.github.com>
2019-07-01 05:38:22 +0300

    fixing speed issues

--- src/Scene.js
+++ src/Scene.js
@@ -2,14 +2,12 @@ import "./index.css";
 import React, { Component } from "react";
 import * as THREE from "three";
 import Orbitcontrols from "three-orbitcontrols";
-import Stats from "./common/threejslibs/stats.min.js";
 
 class Scene extends Component {
   componentDidMount() {
     this.initThree();
   }
   initThree() {
-    let stats;
     let camera, scene, renderer;
     // let group;
     let container = document.getElementById("WebGL-output");
@@ -17,14 +15,15 @@ class Scene extends Component {
       height = container.clientHeight;
     let windowHalfX = window.innerWidth / 2,
       windowHalfY = window.innerHeight / 2;
+    let orbitControls;
 
     let loadingScreen = {
       scene: new THREE.Scene(),
-      camera: new THREE.PerspectiveCamera(60, width / height, 1, 2000),
-      box: new THREE.Mesh(
-        new THREE.BoxGeometry(0.5, 0.5, 0.5),
-        new THREE.MeshBasicMaterial({ color: 0x4444ff })
-      )
+      camera: new THREE.PerspectiveCamera(60, width / height, 1, 2000)
+      // box: new THREE.Mesh(
+      //   new THREE.BoxGeometry(0.5, 0.5, 0.5),
+      //   new THREE.MeshBasicMaterial({ color: 0x4444ff })
+      // )
     };
 
     let loadingManager = null;
@@ -40,9 +39,9 @@ class Scene extends Component {
       // scene.add(group);
 
       // Create a loading screen
-      loadingScreen.box.position.set(0, 0, 5);
-      loadingScreen.camera.lookAt(loadingScreen.box.position);
-      loadingScreen.scene.add(loadingScreen.box);
+      // loadingScreen.box.position.set(0, 0, 5);
+      // loadingScreen.camera.lookAt(loadingScreen.box.position);
+      // loadingScreen.scene.add(loadingScreen.box);
       loadingManager = new THREE.LoadingManager();
       loadingManager.onProgress = function() {};
       loadingManager.onLoad = function() {
@@ -50,19 +49,26 @@ class Scene extends Component {
       };
 
       // Init camera and perspective
-      camera = new THREE.PerspectiveCamera(60, width / height, 1, 2000);
+      camera = new THREE.PerspectiveCamera(65, width / height, 1, 2000);
       camera.position.x = -10;
       camera.position.y = 15;
       camera.position.z = 500;
       camera.lookAt(scene.position);
 
       // Orbitcontrols
-      let orbitControls = new Orbitcontrols(camera);
-      orbitControls.autoRotate = false;
+      orbitControls = new Orbitcontrols(camera);
+      orbitControls.autoRotate = true;
+      orbitControls.autoRotateSpeed = 0.2;
       orbitControls.enableZoom = false;
+      orbitControls.enableDamping = true;
+      orbitControls.dampingFactor = 0.15; // friction factor
+      orbitControls.rotateSpeed = 0.1; // mouse sensitivity
 
-      let ambi = new THREE.AmbientLight(0xffffff);
-      scene.add(ambi);
+      let ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+      scene.add(ambientLight);
+
+      let pointLight = new THREE.PointLight(0xffffff, 0.5);
+      scene.add(pointLight);
 
       // Texture
       let planetLoader = new THREE.TextureLoader(loadingManager);
@@ -79,9 +85,10 @@ class Scene extends Component {
         let mesh = new THREE.Mesh(sphereGeometry, material);
         scene.add(mesh);
 
-        let outline = new THREE.MeshBasicMaterial({
+        let outline = new THREE.MeshLambertMaterial({
           color: 0xffffff,
-          side: THREE.BackSide
+          side: THREE.BackSide,
+          emissive: 0xffffff
         });
         let outlineMesh = new THREE.Mesh(sphereGeometry, outline);
         outlineMesh.scale.multiplyScalar(1.01);
@@ -107,8 +114,6 @@ class Scene extends Component {
       renderer.setPixelRatio(window.devicePixelRatio);
       renderer.setSize(width, height);
       container.appendChild(renderer.domElement);
-      stats = new Stats();
-      container.appendChild(stats.dom);
 
       window.addEventListener("resize", onWindowResize, false);
     }
@@ -116,22 +121,15 @@ class Scene extends Component {
     function animate() {
       if (RESOURCES_LOADED === false) {
         requestAnimationFrame(animate);
-
-        loadingScreen.box.position.x -= 0.05;
-        if (loadingScreen.box.position.x < -10)
-          loadingScreen.box.position.x = 10;
-        loadingScreen.box.position.y = Math.sin(loadingScreen.box.position.x);
-
         renderer.render(loadingScreen.scene, loadingScreen.camera);
         return; // Stop the function here.
       }
       requestAnimationFrame(animate);
+      orbitControls.update(); // required when damping is enabled; otherwise, not required
       render();
-      stats.update();
     }
     function render() {
-      scene.rotation.y -= 0.005;
-      // scene.background
+      scene.rotation.y += 0.002;
       renderer.render(scene, camera);
     }
     function onWindowResize() {
@@ -143,7 +141,13 @@ class Scene extends Component {
     }
   }
   render() {
-    return <div id="WebGL-output" />;
+    return (
+      <div id="WebGL-output">
+        <div className="overlay">
+          <h1 className="title">WEB DESIGN</h1>
+        </div>
+      </div>
+    );
   }
 }
 
